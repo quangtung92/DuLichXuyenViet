2cc8072b2c2d76a7f051aa9a0f930194f970dd81
quangtung92 <life_is_not_waiting@yahoo.com>
2016-05-05 08:27:22 +0700

    first release
    first release with 1 function: loading HCMC map with navigation (zoom
    and pan)

---
+++ app/src/main/java/com/release/tony/dulichxuyenviet/MapActivity.java
@@ -0,0 +1,32 @@
+package com.release.tony.dulichxuyenviet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        MapView mapView = (MapView) findViewById(R.id.mapView);
+        mapView.setClickable(true);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+        mapView.setUseDataConnection(true);
+        mapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
+
+        GeoPoint HCMC = new GeoPoint(10.790141, 106.677246);
+
+        IMapController mapViewController = mapView.getController();
+        mapViewController.setZoom(11);
+        mapViewController.setCenter(HCMC);
+    }
+}
